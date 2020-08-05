9158fd8746a066e76c4dac262351370d20e03344
Jose Luis Martinez <jmartinez@zarotransportation.com>
2020-08-05 01:08:31 -0500

    +migration opt

--- database/migrations/1596605502354_optics_schema.js
+++ database/migrations/1596605502354_optics_schema.js
@@ -7,6 +7,14 @@ class OpticsSchema extends Schema {
   up () {
     this.create('optics', (table) => {
       table.increments()
+      table.integer('manager_id',).unsigned().references('id').inTable('users')
+      table.string('opti_name', 80).notNullable()
+      table.string('direction', 300).notNullable()
+      table.string('city', 120).notNullable()
+      table.string('RFC', 14).notNullable()
+      table.string('description', 300).notNullable()
+      table.string('tel', 17)
+      table.string('logo_img')
       table.timestamps()
     })
   }
